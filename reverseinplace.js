07b5976171c9a9a60567918fd013f4196b90ddc1
amyruth <anrutherford@gmail.com>
2015-08-27 21:54:38 -0400

    removed redundant join added comments

--- reverseinplace.js
+++ reverseinplace.js
@@ -1,10 +1,16 @@
+//handles sentences as well as single words
+
 var reverseInPlace = function(string){
+  //turn the string into an array of words
   var splitstring = string.split(" ");
+  //array for the words as they're being reversed
   var reverseArray = [];
   
   for(var i =0; i < splitstring.length; i+=1){
+    //removes 1st word from array
     var word = splitstring.splice(0,1);
-    word.join();
+    //turn it into a string, then splits into individual letters
+    //reverse array and rejoins as string
     word = word.join().split("").reverse().join("");
     reverseArray.push(word);
     i-= 1;
